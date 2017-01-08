603a79bbf3b1158d56090c82e4d957cacfe59584
efanov_evgeniy <efanovev1995@gmail.com>
2017-01-08 17:27:58 +0300

    Fix AuthActivity

--- app/src/main/java/com/academiaexpress/Activities/AuthActivity.kt
+++ app/src/main/java/com/academiaexpress/Activities/AuthActivity.kt
@@ -21,51 +21,76 @@ import com.academiaexpress.Base.BaseActivity
 import com.academiaexpress.R
 import com.academiaexpress.Server.DeviceInfoStore
 import com.academiaexpress.Utils.AndroidUtilities
+import com.academiaexpress.Utils.AppConfig
 import com.academiaexpress.Utils.Image
 
 class AuthActivity : BaseActivity() {
 
+    private val DEFAULT_COLOR = "#212121"
+    private val EXTRA_PHONE = "phone"
+    private val EXTRA_FIRST = "first"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.email_screen)
+        setContentView(R.layout.activity_phone_input)
 
+        resetData()
+        initElements()
+        setOnClickListeners()
+    }
+
+    private fun resetData() {
         DeviceInfoStore.resetToken(this)
         DeviceInfoStore.resetUser(this)
+    }
 
-        (findViewById(R.id.phone) as EditText).addTextChangedListener(PhoneNumberFormattingTextWatcher())
+    private fun initSpan() {
+        val span = Spannable.Factory.getInstance().newSpannable("Нажимая на кнопку \"ДАЛЕЕ\", вы принимаете правила")
+        span.setSpan(object : TouchableSpan(Color.parseColor(DEFAULT_COLOR), Color.parseColor(DEFAULT_COLOR), Color.TRANSPARENT) {
+            override fun onClick(v: View) { browseTerms() } }, 0, span.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
 
-        val span = Spannable.Factory.getInstance().newSpannable(
-                "Нажимая на кнопку \"ДАЛЕЕ\", вы принимаете правила")
-        span.setSpan(object : TouchableSpan(
-                Color.parseColor("#212121"),
-                Color.parseColor("#212121"),
-                Color.TRANSPARENT) {
-            override fun onClick(v: View) {
-                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://academia-delivery.herokuapp.com/docs/terms_of_service.html"))
-                startActivity(browserIntent)
-            }
-        }, 0, span.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         (findViewById(R.id.textView61) as TextView).movementMethod = LinkTouchMovementMethod()
         (findViewById(R.id.textView61) as TextView).text = span
+    }
 
+    private fun browseTerms() {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(AppConfig.termsUrl))
+        startActivity(browserIntent)
+    }
+
+    private fun initElements() {
         Image.loadPhoto(R.drawable.back1, findViewById(R.id.imageView21) as ImageView)
+        (findViewById(R.id.phone) as EditText).addTextChangedListener(PhoneNumberFormattingTextWatcher())
 
+        initSpan()
+    }
+
+    private fun setOnClickListeners() {
         findViewById(R.id.textView7).setOnClickListener(View.OnClickListener {
             if (!AndroidUtilities.isConnected(this@AuthActivity)) return@OnClickListener
-
-            if ((findViewById(R.id.phone) as EditText).text.toString().length < 5) {
-                Snackbar.make(findViewById(R.id.main), "Неверный формат номера.", Snackbar.LENGTH_SHORT).show()
-                return@OnClickListener
-            }
-
-            val intent = Intent(this@AuthActivity, CodeActivity::class.java)
-            intent.putExtra("first", true)
-            intent.putExtra("phone", (findViewById(R.id.phone) as EditText).text.toString())
-            startActivity(intent)
+            if (!check()) return@OnClickListener
+            openCodeActivity()
         })
     }
 
-    abstract inner class TouchableSpan(private val mNormalTextColor: Int, private val mPressedTextColor: Int, private val mPressedBackgroundColor: Int) : ClickableSpan() {
+    private fun check(): Boolean {
+        if ((findViewById(R.id.phone) as EditText).text.toString().length < 5) {
+            Snackbar.make(findViewById(R.id.main), getString(R.string.wrong_phone_format), Snackbar.LENGTH_SHORT).show()
+            return false
+        }
+
+        return true
+    }
+
+    private fun openCodeActivity() {
+        val intent = Intent(this@AuthActivity, CodeActivity::class.java)
+        intent.putExtra(EXTRA_FIRST, true)
+        intent.putExtra(EXTRA_PHONE, (findViewById(R.id.phone) as EditText).text.toString())
+        startActivity(intent)
+    }
+
+    abstract inner class TouchableSpan(private val mNormalTextColor: Int, private val mPressedTextColor: Int,
+                                       private val mPressedBackgroundColor: Int) : ClickableSpan() {
         private var mIsPressed: Boolean = false
 
         fun setPressed(isSelected: Boolean) {
@@ -84,41 +109,45 @@ class AuthActivity : BaseActivity() {
         private var mPressedSpan: TouchableSpan? = null
 
         override fun onTouchEvent(textView: TextView, spannable: Spannable, event: MotionEvent): Boolean {
-            if (event.action == MotionEvent.ACTION_DOWN) {
-                mPressedSpan = getPressedSpan(textView, spannable, event)
-                if (mPressedSpan != null) {
-                    mPressedSpan!!.setPressed(true)
-                    Selection.setSelection(spannable, spannable.getSpanStart(mPressedSpan),
-                            spannable.getSpanEnd(mPressedSpan))
-                }
-            } else if (event.action == MotionEvent.ACTION_MOVE) {
-                val touchedSpan = getPressedSpan(textView, spannable, event)
-                if (mPressedSpan != null && touchedSpan !== mPressedSpan) {
-                    mPressedSpan!!.setPressed(false)
-                    mPressedSpan = null
-                    Selection.removeSelection(spannable)
-                }
-            } else {
-                if (mPressedSpan != null) {
-                    mPressedSpan!!.setPressed(false)
-                    super.onTouchEvent(textView, spannable, event)
-                }
-                mPressedSpan = null
-                Selection.removeSelection(spannable)
-            }
+            if (event.action == MotionEvent.ACTION_DOWN) processDown(spannable)
+            else if (event.action == MotionEvent.ACTION_MOVE) processMove(textView, spannable, event)
+            else processStandart(textView, spannable, event)
+
             return true
         }
 
-        private fun getPressedSpan(textView: TextView, spannable: Spannable, event: MotionEvent): TouchableSpan {
+        private fun processStandart(textView: TextView, spannable: Spannable, event: MotionEvent) {
+            if (mPressedSpan != null) {
+                mPressedSpan!!.setPressed(false)
+                super.onTouchEvent(textView, spannable, event)
+            }
+            mPressedSpan = null
+            Selection.removeSelection(spannable)
+        }
 
+        private fun processMove(textView: TextView, spannable: Spannable, event: MotionEvent) {
+            val touchedSpan = getPressedSpan(textView, spannable, event)
+            if (mPressedSpan != null && touchedSpan !== mPressedSpan) {
+                mPressedSpan!!.setPressed(false)
+                mPressedSpan = null
+                Selection.removeSelection(spannable)
+            }
+        }
+
+        private fun processDown(spannable: Spannable) {
+            if (mPressedSpan != null) {
+                mPressedSpan!!.setPressed(true)
+                Selection.setSelection(spannable, spannable.getSpanStart(mPressedSpan),
+                        spannable.getSpanEnd(mPressedSpan))
+            }
+        }
+
+        private fun getPressedSpan(textView: TextView, spannable: Spannable, event: MotionEvent): TouchableSpan {
             var x = event.x.toInt()
             var y = event.y.toInt()
 
-            x -= textView.totalPaddingLeft
-            y -= textView.totalPaddingTop
-
-            x += textView.scrollX
-            y += textView.scrollY
+            x = setX(x, textView)
+            y = setY(y, textView)
 
             val layout = textView.layout
             val line = layout.getLineForVertical(y)
@@ -126,11 +155,28 @@ class AuthActivity : BaseActivity() {
 
             val link = spannable.getSpans(off, off, TouchableSpan::class.java)
             var touchedSpan: TouchableSpan? = null
-            if (link.isNotEmpty()) {
-                touchedSpan = link[0]
-            }
+
+            if (link.isNotEmpty()) touchedSpan = link[0]
+
             return touchedSpan!!
         }
 
+        private fun setX(x: Int, textView: TextView): Int {
+            var newX = x
+
+            newX -= textView.totalPaddingLeft
+            newX += textView.scrollX
+
+            return newX
+        }
+
+        private fun setY(y: Int, textView: TextView): Int {
+            var newY = y
+
+            newY -= textView.totalPaddingTop
+            newY += textView.scrollY
+
+            return newY
+        }
     }
 }
--- app/src/main/java/com/academiaexpress/Utils/AppConfig.java
+++ app/src/main/java/com/academiaexpress/Utils/AppConfig.java
@@ -2,4 +2,6 @@ package com.academiaexpress.Utils;
 
 public class AppConfig {
     public static final String LogTag = "AcademiaExpress";
+
+    public static final String termsUrl = "https://academia-delivery.herokuapp.com/docs/terms_of_service.html";
 }
