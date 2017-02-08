0c4b9a1da37b21aa4cd259c8758a13c8aa69ea5f
Efanov Evgeny <efanovev1995@gmail.com>
2017-02-08 23:54:01 +0300

    Add Firebase push notifications (#9)
    * Remove volley from project
    
    * Add firebase and fix manifest
    
    * Send device token to server
    
    * Update gitignore
    
    * Remove gitignore from app folder

--- app/src/main/java/com/academiaexpress/Push/MyGcmListenerService.java
+++ app/src/main/java/com/academiaexpress/Push/MyGcmListenerService.java
@@ -1,22 +1,4 @@
-/**
- * Copyright 2015 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.academiaexpress.Push;
-
-import com.google.android.gms.gcm.GcmListenerService;
 
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -24,70 +6,35 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
 
 import com.academiaexpress.Activities.SplashActivity;
 import com.academiaexpress.R;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
-public class MyGcmListenerService extends GcmListenerService {
-
-    private static final String TAG = "MyGcmListenerService";
-
-    /**
-     * Called when message is received.
-     *
-     * @param from SenderID of the sender.
-     * @param data Data bundle containing message data as key/value pairs.
-     *             For Set of keys use data.keySet().
-     */
-    // [START receive_message]
+public class MyGcmListenerService extends FirebaseMessagingService {
     @Override
-    public void onMessageReceived(String from, Bundle data) {
-        String message = data.getString("message");
-        Log.d(TAG, "From: " + from);
-        Log.d(TAG, "Message: " + message);
-
-        if (from.startsWith("/topics/")) {
-            // message received from some topic.
-        } else {
-            // normal downstream message.
-        }
-
-        // [START_EXCLUDE]
-        /**
-         * Production applications would usually process the message here.
-         * Eg: - Syncing with server.
-         *     - Store message in local database.
-         *     - Update UI.
-         */
-
-        /**
-         * In some cases it may be useful to show a notification indicating to the user
-         * that a message was received.
-         */
-        sendNotification(message);
-        // [END_EXCLUDE]
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        processPush(remoteMessage);
     }
-    // [END receive_message]
 
-    /**
-     * Create and show a simple notification containing the received GCM message.
-     *
-     * @param message GCM message received.
-     */
-    private void sendNotification(String message) {
+    private void processPush(RemoteMessage remoteMessage) {
+        String body = remoteMessage.getNotification() == null || remoteMessage.getNotification().getBody() == null ?
+                getString(R.string.default_notification_text) : remoteMessage.getNotification().getBody();
+        sendNotification(body);
+    }
+
+    private void sendNotification(String messageBody) {
         Intent intent = new Intent(this, SplashActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-                PendingIntent.FLAG_ONE_SHOT);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("Академия Экспресс")
-                .setContentText(message)
+                .setSmallIcon(R.drawable.ic_plus)
+                .setContentTitle(getString(R.string.order_status_changed))
+                .setContentText(messageBody)
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent);
@@ -95,6 +42,6 @@ public class MyGcmListenerService extends GcmListenerService {
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        notificationManager.notify(0, notificationBuilder.build());
     }
 }
