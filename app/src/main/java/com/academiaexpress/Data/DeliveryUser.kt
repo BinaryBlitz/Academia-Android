b018082e6e379387dcc3c14ac532ba6ab80e1777
outboard <efanovev1995@gmail.com>
2017-04-16 13:56:07 +0300

    Refactor user class

--- app/src/main/java/com/academiaexpress/Data/DeliveryUser.kt
+++ app/src/main/java/com/academiaexpress/Data/DeliveryUser.kt
@@ -1,22 +1,28 @@
 package com.academiaexpress.Data
 
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+
 class DeliveryUser(var firstName: String?, var lastName: String?, var email: String?, var phoneNumber: String?) {
     fun asString(): String {
-        return firstName + splitter + lastName + splitter + email + splitter + phoneNumber
+        val userObject = JsonObject()
+        userObject.addProperty("firstName", firstName)
+        userObject.addProperty("lastName", lastName)
+        userObject.addProperty("email", email)
+        userObject.addProperty("phoneNumber", phoneNumber)
+        return userObject.toString()
     }
 
     companion object {
-        val splitter = "DeliveryUser"
-        val FIRST_NAME_INDEX = 0
-        val LAST_NAME_INDEX = 1
-        val EMAIL_INDEX = 2
-        val PHONE_NUMBER_INDEX = 3
-
         fun fromString(string: String): DeliveryUser? {
             try {
-                // get string array from preferences
-                val fields = string.split(splitter.toRegex()).dropLastWhile(String::isEmpty).toTypedArray()
-                return DeliveryUser(fields[FIRST_NAME_INDEX], fields[LAST_NAME_INDEX], fields[EMAIL_INDEX], fields[PHONE_NUMBER_INDEX])
+                val jsonReader = JsonParser()
+                val userObject = jsonReader.parse(string) as JsonObject
+
+                return DeliveryUser(userObject.get("firstName").asString,
+                        userObject.get("lastName").asString,
+                        userObject.get("email").asString,
+                        userObject.get("phoneNumber").asString)
             } catch (e: Exception) {
                 return  null
             }
